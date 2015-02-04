d70c2ab3acb775ebe6493b079acb54e69afb8c1f
Weimin Liu <wei@Weimins-MacBook-Air.local>
2015-02-04 14:40:52 -0500

    Initial commit

---
+++ src/org/waspec/Main.java
@@ -0,0 +1,26 @@
+package org.waspec;
+
+public class Main {
+
+    public static void main(String[] args){
+        double x = 140;
+        double y = 4;
+        double expression;
+        expression = x/y;
+        System.out.println(expression);
+    }
+}
+
+class Vehicle{
+    public double speed;
+}
+ class Truck extends Vehicle{
+     public double payload;
+ }
+
+class Car extends Vehicle{
+    public int passenger;
+}
+
+class Tank extends Vehicle{
+};
