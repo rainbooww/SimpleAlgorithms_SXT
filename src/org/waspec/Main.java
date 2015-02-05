6f9f28793349d3f0d7cb5275c7f76e1addd7087e
wei <weiminliu.rx@gmail.com>
2015-02-05 00:25:11 -0500

    while  循环

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -3,9 +3,12 @@ package org.waspec;
 public class Main {
     public static void main(String[] args) {
         int x = 0;
-        while (x<=99) {
+        while (x<=100) {
             x++;
             System.out.println(x);
+            if (x>99) {
+                break;
+            }
         }
     }
 }
