0d2455d81a84e457562cec45ace55bcca46c186a
wei <weiminliu.rx@gmail.com>
2015-02-05 00:08:18 -0500

    intScore

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,11 +2,25 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
-        if (x > 99){
-            System.out.println("yes");}// 若需要多条语句，加花括号
-        else
-        {System.out.println("no");
-            System.out.println("不行");}
-    }
-}
+        int score =88;
+        if (score>=0 && score <40) {
+            System.out.println("D");
+        }else{
+            if(score>=40 && score<60) {
+                System.out.println("C");
+            }
+            else {
+                if (score>=60 && score <80) {
+                    System.out.println("B");
+                }else{
+                    if (score>=80 && score <=100) {
+                        System.out.println("A");
+                    }else {
+                        System.out.println("ERROR");
+                    }
+                    }
+                }
+            }
+        }
+        }
+
