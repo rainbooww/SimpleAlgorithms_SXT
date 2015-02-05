5b6b569f0cd0d11925b9bd3cd641632a0bffaf63
wei <weiminliu.rx@gmail.com>
2015-02-04 23:42:35 -0500

    Update, must explain reason

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,29 +1,30 @@
 package org.waspec;
 
 public class Main {
-
-    public static void main(String[] args){
-        double x = 140;
-        double y = 4;
-        double expression;
-        expression = x/y;
-        System.out.println(expression);
-        System.out.println("I am a happy developer");
-        System.out.println("I love coding");
-        System.out.println("Let's push code");
+    public static void main(String[] args) {
+        (new Television()).play();
+        double x= 1.1;
+        double y= 2.2;
+        double result = 0;
+        result = Calculator.absAdd(2.2,3.3);//
     }
 }
 
-class Vehicle{
-    public double speed;
-}
- class Truck extends Vehicle{
-     public double payload;
- }
-
-class Car extends Vehicle{
-    public int passenger;
+class Television{
+    public void play(){
+        System.out.println("CCTV channel 10.");
+    }
 }
 
-class Tank extends Vehicle{
-};
+class Calculator{
+    public static double absAdd(double a, double b){
+        double result = a+b;
+        System.out.println(result);
+        if (result>=0){
+            return result;
+        }else{
+            return -result;
+        } //可以有多个return
+
+    }
+}
