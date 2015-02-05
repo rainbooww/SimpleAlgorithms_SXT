fdca5449e13f1bfe6d38664d4b014460a9451d6e
wei <weiminliu.rx@gmail.com>
2015-02-05 00:15:40 -0500

    if....else if...else

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -3,24 +3,17 @@ package org.waspec;
 public class Main {
     public static void main(String[] args) {
         int score =88;
-        if (score>=0 && score <40) {
+        if (score>=0 & score <40) {
             System.out.println("D");
-        }else{
-            if(score>=40 && score<60) {
+        }else if (score>=40 & score<60) {
                 System.out.println("C");
-            }
-            else {
-                if (score>=60 && score <80) {
+            } else if (score>=60 & score <80) {
                     System.out.println("B");
-                }else{
-                    if (score>=80 && score <=100) {
+                }else if (score>=80 & score <=100) {
                         System.out.println("A");
                     }else {
                         System.out.println("ERROR");
                     }
-                    }
                 }
-            }
-        }
-        }
 
+}
