9227963347244ce7d457658f146a57f4a0528eb2
wei <weiminliu.rx@gmail.com>
2015-02-07 22:49:11 -0500

    0-12内的奇数

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,9 +1,9 @@
 package org.waspec;
 public class Main {
     public static void main(String[] args) {
-       int skip=4;
-        for (int i=0; i<=5;i++) {
-            if (i==skip) continue;
+      /* int skip=4;*/
+        for (int i=1; i<=12;i+=2) {
+            /*if (i==skip) continue;*/
             System.out.println("i="+i);
 
             }
