25b039e4b8c4a4fb94f60e394eb40455613940eb
wei <weiminliu.rx@gmail.com>
2015-02-07 22:44:03 -0500

    1到5的打印，当i＝4，跳过循环（skip）

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,14 +1,14 @@
 package org.waspec;
 public class Main {
     public static void main(String[] args) {
-       int i=0;
-        while (true) {
-            System.out.println(i++);
-            if (i>99){
-                break;
+       int skip=4;
+        for (int i=0; i<=5;i++) {
+            if (i==skip) continue;
+            System.out.println("i="+i);
+
             }
         }
-        }
-        }
+}
+
 
 
