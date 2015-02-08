0f981b36a223c71a7adcda0b7f0385d69e9d3949
wei <weiminliu.rx@gmail.com>
2015-02-07 22:37:36 -0500

    1-99的循环， while...break

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,18 +1,14 @@
 package org.waspec;
-
 public class Main {
     public static void main(String[] args) {
-        Calculator.add(9.9,1.1);
-        double r= Calculator.add(3.3,4.4);
-        System.out.println(r);
-    }
-}
+       int i=0;
+        while (true) {
+            System.out.println(i++);
+            if (i>99){
+                break;
+            }
+        }
+        }
+        }
 
-class Calculator{
-    public static double add(double a,double b){
-        double result = a+b;
-        System.out.println(result);
-        return result;
-    }
 
-}
