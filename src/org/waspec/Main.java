b0f5665b552fe215bcf36d0fd4af212d8169c329
wei <weiminliu.rx@gmail.com>
2015-02-07 23:29:49 -0500

    圆面积两种写法 1/2

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,13 +1,22 @@
 package org.waspec;
 public class Main {
     public static void main(String[] args) {
-        int j = 0;
-        for (int i = 1; i < 100; i += 2) {
-            j += i;
-        }
-            System.out.println("result=" + j);
+        double r =10;
+        double area = Calculator.getCircleArea(10);
+        System.out.println(area);
+
+    }
+}
+
+    class Calculator {
+        double r;
+        static double getCircleArea(double r) {
+            double area;
+            return area = Math.PI * r * r;
         }
     }
+
+
 
 
 
