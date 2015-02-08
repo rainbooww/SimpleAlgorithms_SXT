c1d84cebc82f121c89c9bd267d64d5d440469f40
wei <weiminliu.rx@gmail.com>
2015-02-07 23:01:23 -0500

    1+2+3+.....+99 (for 循环）

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,14 +1,15 @@
 package org.waspec;
 public class Main {
     public static void main(String[] args) {
-      /* int skip=4;*/
-        for (int i=1; i<=12;i+=2) {
-            /*if (i==skip) continue;*/
-            System.out.println("i="+i);
-
-            }
+        int j = 0;
+        for (int i = 1; i < 100; i++) {
+            j += i;
         }
-}
+            System.out.println("result=" + j);
+
+        }
+    }
+
 
 
 
