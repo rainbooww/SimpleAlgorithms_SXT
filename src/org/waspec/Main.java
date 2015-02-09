b40aa0f8c1be2bd42475ee9f39a4c61779c139a0
wei <weiminliu.rx@gmail.com>
2015-02-08 22:37:55 -0500

    9 9 乘法表，2个 for 循环嵌套

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,9 +2,9 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("[%dx%d=%d]\t",i,j,i * j);
             }
         }
     }
