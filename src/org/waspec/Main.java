5f997a70142238728ac583c9f56b33d2c8fe15cd
wei <weiminliu.rx@gmail.com>
2015-02-08 22:20:35 -0500

    for 循环打印 1- 20内偶数

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,10 +2,12 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        do {
-            System.out.println(i++);}
-           /* i++;*/
-            while (i<20);
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
+
+
