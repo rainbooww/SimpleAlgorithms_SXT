8b235a87f46a487534a3c6de40caf361a939b3ae
wei <weiminliu.rx@gmail.com>
2015-02-08 22:44:20 -0500

    for  循环打印三角形

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,10 +2,11 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.printf("[%dx%d=%d]\t",i,j,i * j);
+                System.out.print("@"+"\t");
             }
+            System.out.println();
         }
     }
 }
