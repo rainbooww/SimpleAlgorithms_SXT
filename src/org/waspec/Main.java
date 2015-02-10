c8b571b8821761f068d5e2be181bdbdf452b0b8c
wei <weiminliu.rx@gmail.com>
2015-02-09 21:38:33 -0500

    数组对调

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,18 +2,15 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j < n - 1 - i) {
-                    System.out.print(' ');
-                } else {
-                    System.out.print("@");
-                }
-            }
-                System.out.println();
+        int [] myArray= {1,3,2,4,7,8,0,6};
+        for (int i = 0; i <(myArray.length)/2 ; i++) {
+            int temp= myArray[i];
+            myArray[i]= myArray[myArray.length-1-i];
+            myArray[myArray.length-1-i]=temp;
         }
-    }
-}
+        System.out.println(myArray);
+        }
+
+        }
 
 
