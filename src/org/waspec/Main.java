43e22918d2031c0ca3b14702321d5831700d216c
wei <weiminliu.rx@gmail.com>
2015-02-09 21:47:12 -0500

    数组对调第二种写法 leftIndex, rightIndex

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -3,10 +3,14 @@ package org.waspec;
 public class Main {
     public static void main(String[] args) {
         int [] myArray= {1,3,2,4,7,8,0,6};
-        for (int i = 0; i <(myArray.length)/2 ; i++) {
-            int temp= myArray[i];
-            myArray[i]= myArray[myArray.length-1-i];
-            myArray[myArray.length-1-i]=temp;
+        int leftIndex=0;
+        int rightIndex= myArray.length-1;
+        while(leftIndex<=rightIndex){
+            int temp= myArray[leftIndex];
+            myArray[leftIndex]= myArray[rightIndex];
+            myArray[rightIndex]=temp;
+            leftIndex++;
+            rightIndex--;
         }
         System.out.println(myArray);
         }
