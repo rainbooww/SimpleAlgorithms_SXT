c8c586d3b402fa3cb21792ce2ca002d863f93760
wei <weiminliu.rx@gmail.com>
2015-02-11 00:29:31 -0500

    数组打印棋盘

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,19 +2,19 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        char[] chars = {'a', 'b', 'c'};
-        String result = new String(chars);
-        System.out.println(result);
+        int n = 8;
+        char [][] board = new char[n][n];
+        for (int row = 0; row < n; row++) {
+            for (int cell = 0; cell < n; cell++) {
+                board[row][cell] = (row+cell)%2==0?'x':'0';
+            }
+        }
+        for (int row = 0; row < n; row++) {
+            String rowString = new String(board[row]);
+            System.out.println(rowString);
+        }
     }
 }
-    /*0 static void main(String[] args) {
-        int n=8;
-        int[][]board = new int[n][n];
-        for (int row = 0; row <n ; row++) {
-            for (int cell = 0; cell <n ; cell++) {
-                board[row][cell]=10;
-            }
-        }*/
 
 
 
