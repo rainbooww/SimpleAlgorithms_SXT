c936eed35ca9b3c081792bcc15a465a279e6c49e
wei <weiminliu.rx@gmail.com>
2015-02-11 13:02:38 -0500

    打印二维数组里的每个元素的位置

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,16 +2,25 @@ package org.waspec;
 
 public class Main {
     public static void main (String[] args){
-        int a [][]={{1,2},{3,4,5,6},{7,8,9}};
-        for (int i = 0; i <a.length ; i++) { //第一维数组里的3个数组循环
-            for (int j = 0; j < a[i].length; j++) { //第二维数组里的元素进行循环
-                System.out.print("a[" + i + "][" + j + "]=" + a[i][j] + "  ");
+       String [][] s;
+        s= new String[3][];
+        s[0]= new String[2];
+        s[1]= new String[3];
+        s[2]= new String[2];
+        for (int i = 0; i <s.length; i++) {
+            for (int j = 0; j <s[i].length ; j++) {
+                s[i][j]= new String(" my position is: "+i+","+j);
             }
-            System.out.println();//把第一维数组里的每个数组分行
+        }
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < s[i].length; j++) {
+                System.out.print(s[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-}
 
+}
 
 
 
