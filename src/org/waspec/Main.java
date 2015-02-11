0b807f4bab1c01335e15bcff19144c468bce09f8
wei <weiminliu.rx@gmail.com>
2015-02-11 12:51:55 -0500

    打印二维数组里的每个元素

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,20 +1,18 @@
 package org.waspec;
 
 public class Main {
-    public static void main(String[] args) {
-        int n = 8;
-        char [][] board = new char[n][n];
-        for (int row = 0; row < n; row++) {
-            for (int cell = 0; cell < n; cell++) {
-                board[row][cell] = (row+cell)%2==0?'x':'0';
+    public static void main (String[] args){
+        int a [][]={{1,2},{3,4,5,6},{7,8,9}};
+        for (int i = 0; i <a.length ; i++) { //第一维数组里的3个数组循环
+            for (int j = 0; j < a[i].length; j++) { //第二维数组里的元素进行循环
+                System.out.print("a[" + i + "][" + j + "]=" + a[i][j] + "  ");
             }
-        }
-        for (int row = 0; row < n; row++) {
-            String rowString = new String(board[row]);
-            System.out.println(rowString);
+            System.out.println();//把第一维数组里的每个数组分行
         }
     }
 }
+
+
 
 
 
