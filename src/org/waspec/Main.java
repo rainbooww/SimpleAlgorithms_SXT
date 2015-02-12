5c2ab005da6e8a1be81c1e058fc62f80dce85b00
wei <weiminliu.rx@gmail.com>
2015-02-12 00:21:54 -0500

    构造器举例

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,24 +2,24 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        Node  node= new Node(10);
-        node.next= new Node(20);
-        node.next.next= new Node (30);
-        node.next.next.next= new Node (40);
-
-        Node current=node;
-        do{
-            System.out.println(current.payload);
-            current = current.next;
-        } while (current!=null);
+        Zombie[] zombies = new Zombie[5];
+        for (int i = 0; i < zombies.length; i++) {
+            zombies[i]= new Zombie(i*100);
+        }
+        for (Zombie zombie: zombies) {
+            zombie.age += 10;
+        }
+        for (int i = 0; i < zombies.length; i++) {
+            System.out.println(zombies[i].age);
+        }
     }
 }
-
-class Node{
-    public Node(int payload){
-        this.payload=payload;
+    
+    
+class Zombie{
+    Zombie(int age){
+        this.age=age;
     }
-    public int payload;
-    public Node next;//指向下一个， 单向链表 （面试永远考单向，不会考双向
-    public Node pre; //双向链表
+    public int age;
 }
+    
