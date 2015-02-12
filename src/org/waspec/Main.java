a45caab66fcef7ba40c8e4ea4cd6bdc1e661449a
wei <weiminliu.rx@gmail.com>
2015-02-11 23:59:06 -0500

    node 单链表

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,30 +2,24 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        int score = 100;
-        int x = (score-1)/20;
-            switch (x) {//必须整数类型表达式
+        Node  node= new Node(10);
+        node.next= new Node(20);
+        node.next.next= new Node (30);
+        node.next.next.next= new Node (40);
 
-                case 0:
-                case 1:
-                    System.out.println("d");
-                    break;
-                case 2:
-                    System.out.println("c");
-                    break;
-                case 3:
-                    System.out.println("b");
-                    break;
-                case 4:
-                    System.out.println("a");
-                    break;
-                default:
-                    System.out.println("error");
-                    break;
-
-            }
-
-        }
+        Node current=node;
+        do{
+            System.out.println(current.payload);
+            current = current.next;
+        } while (current!=null);
     }
+}
 
-
+class Node{
+    public Node(int payload){
+        this.payload=payload;
+    }
+    public int payload;
+    public Node next;//指向下一个， 单向链表 （面试永远考单向，不会考双向
+    public Node pre; //双向链表
+}
