5b23b9d0c57601ae83ba84a2dbeb3c91ca29af8e
wei <weiminliu.rx@gmail.com>
2015-02-11 22:30:18 -0500

    数组里出现频率最多的数,第2钟写法

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -3,22 +3,30 @@ package org.waspec;
 public class Main {
     public static void main(String[] args) {
         int[] myArray = {9, 9, 7, 7, 7, 5, 5, 5, 5, 5, 4, 3, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 2, 2};
-        int count = 1;
-        int tempCount;
-        int most = myArray[0];
-        int temp = 0;
-        for (int i = 0; i < myArray.length - 1; i++) {
-            temp = myArray[i];
-            tempCount = 0;
-            for (int j = 1; j < myArray.length; j++) {
-                if (temp == myArray[j])
-                    tempCount++;
-            }
-            if (tempCount > count) {
-                most = temp;
-                count = tempCount;
-            }
-        }
-        System.out.println(most);
+        int result = longestSubArrayLengh(myArray);
+        System.out.println((result));
     }
-}
+    public static int longestSubArrayLengh(int[] input){
+
+        if (input==null||input.length==0){
+            return 0;
+        }
+        int startIndex=0;
+        int endIndex=0;
+        int delta=0;// 变化亮
+        while(endIndex<input.length){
+            int temp=0;
+            if(input[endIndex]==input[startIndex]){
+                temp= endIndex-startIndex;
+                if(temp>delta){
+                    delta=temp;
+                }
+            }else{
+                startIndex= endIndex;
+            }
+            endIndex++;
+        }
+        return delta+1;
+        }
+    }
+
