dd7a3dfdcf10309a043cbe1f2374340a117aadd4
wei <weiminliu.rx@gmail.com>
2015-02-14 22:59:10 -0500

    findMaxLength 自己写出来的

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -2,29 +2,32 @@ package org.waspec;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a={1,1,2,2,3,3};
+        int[] a={1,1,1,3,3,3,5,5,5,5,5};
         System.out.println(findMaxLength(a));
     }
 
-    public static int findMaxLength(int[]a){
-        int startIndex=0;
-        int maxLen=0;
-        int i=0;
-        int currentLen=0;
-        for ( i = 1; i < a.length; i++) {
-            if (a[i]!=a[i-1]){
-                 currentLen= i-startIndex;
-                startIndex=i;
-                if (currentLen>maxLen){
-                    maxLen=currentLen;
-                }
-            }
+    public static int findMaxLength(int[]a) {
+        int startIndex = 0;
+        int endIndex = 0;
+        int currentLen = 0;
+        int maxLen = 0;
+        /*if (a.length==1){
+            return 1;
         }
-           currentLen= i-startIndex;
-           if(currentLen>maxLen){
-               maxLen=currentLen;
-       }
-        return maxLen;
+        if (a.length==0){
+            return 0;
+        }
+*/
+        while (endIndex < a.length) {
+            if (a[startIndex] == a[endIndex]) {
+                currentLen = endIndex - startIndex;
+                maxLen = currentLen;
+            } else {
+                startIndex = endIndex;
+            }
+            endIndex++;
+        }
+    return maxLen+1;
     }
 }
 
