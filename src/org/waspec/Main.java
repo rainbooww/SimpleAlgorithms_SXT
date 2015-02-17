f0a182e672f8e92289b4a4828d6c023510b2a79d
wei <weiminliu.rx@gmail.com>
2015-02-16 23:47:40 -0500

    interface...implements

--- src/org/waspec/Main.java
+++ src/org/waspec/Main.java
@@ -1,5 +1,157 @@
 package org.waspec;
 
+public class Main {
+    public static void main(String[] args) {
+        Weapon myWeapon1 = new RocketLauncher();
+        Weapon myWeapon2 = new Tank();
+        myWeapon1.fire();
+        myWeapon2.fire();
+    }
+}
+
+interface Vehicle {//base interface 基接口，有些为形容词，众多为名词
+    public abstract void run(); // 接口里的方法只有声明，没有定义 没有方法体，不管具体功能的实现, public abstract可不写
+}
+
+interface Weapon{
+    public abstract void fire();
+}
+
+class  Truck implements Vehicle {
+    @Override
+    public void run(){
+        System.out.println("I am running on the highway");
+    }
+}
+
+class Tank implements Vehicle, Weapon {
+    @Override
+    public void run() {
+        System.out.println("I am running on battlefield.");
+    }
+
+    @Override
+    public void fire() {
+        System.out.println("Bang!");
+    }
+}
+
+    class RocketLauncher implements Weapon{
+        @Override
+        public void fire() {
+            System.out.println("3,2,1, launch");
+        }
+    }
+
+
+
+/*public class Main {
+    public static void main(String[] args) {
+        Truck myTruck = new Truck();
+        myTruck.run();
+
+        Tank myTank = new Tank();
+        myTank.run();
+    }
+}
+
+class Truck {
+    public void run(){
+        System.out.println("I am running on the highway");
+    }
+}
+
+class Tank {
+    public void run(){
+        System.out.println("I am running on battlefield.");
+    }
+}*/
+
+
+/*public class Main {
+    public static void main(String[] args) {
+        Student student= new Student(1,"Tim");
+        student.name="Tim";
+        System.out.printf("Student %s's id is %d.\n", student.name, student.id);
+    }
+}
+
+class Student{
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public Student(int id, String name) {//overload of the above method, 省去一行初始化
+        this.id = id;
+        this.name = name;
+    }
+
+    public int id;
+    String name;
+}*/
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator= new Calculator();
+        int x= calculator.minus(5,3);
+        System.out.println(x);
+    }
+}
+
+class Calculator{
+
+    public int minus (int a, int b){
+        int result = a-b;
+        return result;
+    }
+
+    public double minus(double x, double y){
+        double result= x-y;
+        return result;
+    }
+
+    public int minus(int a, int b, int c){
+        int result= a-b-c;
+        return result;
+    }
+}
+*/
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int x = calculator.add();
+        System.out.println(x);
+    }
+}
+
+    class Calculator {
+        public int add(int a, int b) {
+            int result = a + b;
+            return result;
+        }
+
+        public int add(int x, int y, int z ) {
+            int result = x + y + z;
+            return result;
+        }
+
+        public double add(double a, double b){
+            double result = a+b;
+            return result;
+        }
+    }
+*/
+
+
+
+
+
+/*
 public class Main{
     public static void main(String[]args){
         System.out.println(addMethod1(100));
@@ -31,6 +183,7 @@ public class Main{
         return result;
     }
 }
+*/
 
 
 
